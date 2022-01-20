59fd0984504624b22ea37e7e0a8a6ded292a27bc
Amir Malikov <adeptus@protonmail.com>
2022-01-20 23:13:21 +0300

    Added WebCam and BW images

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,21 @@
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char** argv) {
-  if (argc != 2) {
-    printf("usage: DisplayImage.out <Image_Path>\n");
-    return -1;
-  }
   cv::Mat image;
-  image = cv::imread(argv[1], 1);
-  if (!image.data) {
-    printf("No image data \n");
-    return -1;
+
+  cv::VideoCapture cap(0);
+  cv::Mat img;
+  cv::Mat kernel = cv::getStructuringElement(cv::MORPH_DILATE, cv::Size(3, 3));
+
+  while (true) {
+    cap.read(img);
+    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
+    cv::Canny(img, img, 25, 75);
+    cv::dilate(img, img, kernel);
+
+    cv::imshow("Webcam", img);
+    cv::waitKey(1);
   }
-  cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE);
-  cv::imshow("Display Image", image);
-  cv::waitKey(0);
+
   return 0;
 }
