35576fcc7faaac9413d3fabbad67a0de0990bdbc
Amir Malikov <adeptus@protonmail.com>
2022-01-21 21:37:47 +0300

    Added line recognition

--- README.md
+++ README.md
@@ -5,7 +5,14 @@ agraph - area under the graph
 ## TODO
 
 1. [x] reseach how to aprtoximate functions by points
-2. [ ] how openCV work
-   1. [ ] how to search points
-   2. [ ]  need to delete axes
-3. [ ] ...
+2. [x] how openCV work
+3. [ ] how to search points
+4. [ ]  need to delete axes
+5. [ ] Add scaner
+
+### 22 Jan
+
+[ ] Delete vertical
+[ ] Add white line in dil
+[ ] how works threadsholds?
+[ ] make it prettier
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,52 @@
 #include <stdio.h>
+
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char** argv) {
   cv::Mat image;
 
   cv::VideoCapture cap(0);
-  cv::Mat img;
+  cv::Mat img, can, dil, gray, thresh, detected_lines, result;
+  int area;
   cv::Mat kernel = cv::getStructuringElement(cv::MORPH_DILATE, cv::Size(3, 3));
+  std::vector<std::vector<cv::Point>> contours;
+  std::vector<std::vector<cv::Point>> cnts;
+  std::vector<std::vector<cv::Point>> poly;
+
+  cv::Mat horizontal_kernel =
+      cv::getStructuringElement(cv::MORPH_RECT, cv::Size(100, 1));
+
+  cv::Mat repair_kernel =
+      cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1, 6));
 
   while (true) {
     cap.read(img);
-    cv::cvtColor(img, img, cv::COLOR_BGR2GRAY);
-    cv::Canny(img, img, 25, 75);
-    cv::dilate(img, img, kernel);
+    cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
+    cv::Canny(gray, can, 25, 75);
+    cv::dilate(can, dil, kernel);
+
+    // Delete horizontal line
+    cv::morphologyEx(dil, detected_lines, cv::MORPH_OPEN, horizontal_kernel,
+                     cv::Point(-1, -1), 2);
+    cv::findContours(detected_lines, cnts, cv::RETR_EXTERNAL,
+                     cv::CHAIN_APPROX_SIMPLE);
+    cv::drawContours(img, cnts, -1, cv::Scalar(255, 255, 255), 2);
+
+    cv::findContours(dil, contours, cv::RETR_EXTERNAL,
+                     cv::CHAIN_APPROX_TC89_KCOS);
+    poly.resize(contours.size());
+
+    for (size_t i = 0; i < contours.size(); i++) {
+      area = cv::contourArea(contours[i]);
+      if (area < 200) continue;
+
+      cv::approxPolyDP(contours[i], poly[i], 5, false);
+      cv::polylines(img, poly[i], false, cv::Scalar(i * 50, i * 25, 255));
+    }
 
     cv::imshow("Webcam", img);
-    cv::waitKey(1);
+    cv::waitKey(100);
   }
 
   return 0;
