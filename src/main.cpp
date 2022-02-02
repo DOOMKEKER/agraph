3eb3a99eaf5cea39f1e217f0bb2991df4a4350cf
Amir Malikov <adeptus@protonmail.com>
2022-02-03 00:21:17 +0300

    divided the curve into equal sections

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,6 @@
 using points_ar = std::vector<std::vector<cv::Point>>;
 using RotRect = std::vector<cv::Point>;
 
-// struct RotRect {
-//   cv::Point bl;
-//   cv::Point br;
-//   cv::Point ul;
-//   cv::Point ur;
-// };
 
 float w = 522;
 float h = 420;
@@ -27,10 +21,12 @@ void del_line(cv::Mat& input, cv::Mat const& kernel) {
   cv::drawContours(input, cnts, -1, cv::Scalar(0, 0, 0), 3);
 }
 
-void get_rect(RotRect& points, const points_ar& contours, points_ar& poly) {
+void get_rect(RotRect& points, const points_ar& contours) {
   int max_area = 0;
   int area;
   float per;
+  points_ar poly;
+  poly.resize(contours.size());
 
   for (size_t i = 0; i < contours.size(); i++) {
     area = cv::contourArea(contours[i]);
@@ -70,18 +66,23 @@ void img_wrap(cv::Mat& dil, RotRect& Rect, cv::Mat& wrap) {
   cv::Mat matrix = cv::getPerspectiveTransform(src, dst);
   cv::warpPerspective(dil, wrap, matrix, cv::Point(w, h));
 }
+namespace cv {
+bool operator<(cv::Point const& l, cv::Point const& r) { return l.x < r.x; }
+}  // namespace cv
 
 int main(int argc, char** argv) {
   cv::VideoCapture cap(0);
 
-  cv::Mat img, can, dil, gray, gaus, thresh, detected_lines, result, wrap;
+  cv::Mat img, can, dil, gray, gaus, thresh, detected_lines, result, wrap,
+      erode;
   cv::Mat kernel = cv::getStructuringElement(cv::MORPH_DILATE, cv::Size(3, 3));
 
   points_ar contours;
-  points_ar poly;
+  points_ar poly, poly1;
 
+  //  Add trashhold
   cv::Mat horizontal_kernel =
-      cv::getStructuringElement(cv::MORPH_RECT, cv::Size(20, 1)); //  Add trashhold
+      cv::getStructuringElement(cv::MORPH_RECT, cv::Size(20, 1));
 
   cv::Mat vertical_kernel =
       cv::getStructuringElement(cv::MORPH_RECT, cv::Size(1, 25));
@@ -90,24 +91,21 @@ int main(int argc, char** argv) {
   float per;
 
   while (true) {
-    // cap.read(img);
 
     img = cv::imread("../test.jpg");
 
     cv::cvtColor(img, gray, cv::COLOR_BGR2GRAY);
 
-    // if (gray.empty()) continue;
     cv::GaussianBlur(gray, gaus, cv::Size(5, 5), 2);
     cv::Canny(gaus, can, 25, 75);
     cv::dilate(can, dil, kernel);
 
-
     cv::findContours(dil, contours, cv::RETR_EXTERNAL,
-                     cv::CHAIN_APPROX_TC89_KCOS);
+                     cv::CHAIN_APPROX_SIMPLE);
     poly.resize(contours.size());
 
     RotRect Rect;
-    get_rect(Rect, contours, poly);
+    get_rect(Rect, contours);
 
     if (Rect.empty()) continue;
 
@@ -118,18 +116,58 @@ int main(int argc, char** argv) {
     del_line(wrap, horizontal_kernel);
     del_line(wrap, vertical_kernel);
 
-
-    cv::imshow("Webcam1", wrap);
-    cv::waitKey();
-
+    cv::findContours(wrap, contours, cv::RETR_EXTERNAL,
+                     cv::CHAIN_APPROX_NONE);
+    poly1.resize(contours.size());
 
     for (size_t i = 0; i < contours.size(); i++) {
       area = cv::contourArea(contours[i]);
-      if (area < 200) continue;
+      if (area < 50) continue;
 
-      cv::approxPolyDP(contours[i], poly[i], 2, true);
-      cv::polylines(img, poly[i], false, cv::Scalar(i * 50, i * 25, 255));
+      cv::approxPolyDP(contours[i], poly1[i], 0.2, false);
+      cv::polylines(img, poly1[i], false, cv::Scalar(100, 100, 100));
     }
+
+    std::vector<cv::Point> points;
+
+    for (auto& vec : contours) {
+      area = cv::contourArea(vec);
+      if (area < 50) continue;
+      std::cout << vec.size() << std::endl;
+      points.insert(points.end(), vec.begin(), vec.end());
+    }
+
+    auto comp = [](cv::Point& l, cv::Point& r) {return l.x < r.x;};
+
+    std::sort(points.begin(), points.end(), comp);
+
+    std::vector<cv::Point> points2;
+
+    points2.reserve(points.size()/2);
+
+    int j = points[0].x;
+    int c = 0;
+    for (size_t i = 0; i < points.size() - 1; ++i) {
+      int a = points[i].x - j;
+      if (a == 5) {
+        points2.push_back(points[i]);
+        j = points2.back().x;
+      } else if (a > 5) {
+        points2.push_back(points[i]);
+        c = points2.back().x;
+        j = c - (c % 5);
+      }
+    }
+
+    for (auto& i : points2) {
+      cv::circle(img, i, 1, cv::Scalar(255, 255, 255));
+      std::cout << i.x << std::endl;
+    }
+
+    for (size_t i = 0; i < points2.size(); ++i) {
+      cv::circle(img, points2[i], 1, cv::Scalar(255, 0, 0));
+    }
+
 
     cv::imshow("Webcam1", dil);
     cv::imshow("Webcam2", img);
